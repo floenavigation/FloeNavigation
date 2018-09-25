b93192fba2a5931afd72aed386ac8013d8480cc7
floenavigation <floenavigation@gmail.com>
2018-09-25 19:26:34 +0200

    Updated sample measurement activity

--- app/src/main/java/de/awi/floenavigation/sample_measurement/SampleMeasurementActivity.java
+++ app/src/main/java/de/awi/floenavigation/sample_measurement/SampleMeasurementActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.app.Activity;
@@ -24,19 +25,29 @@ import java.util.ArrayList;
 
 import de.awi.floenavigation.DatabaseHelper;
 import de.awi.floenavigation.GPS_Service;
+import de.awi.floenavigation.NavigationFunctions;
 import de.awi.floenavigation.R;
 
 public class SampleMeasurementActivity extends Activity {
 
     private static final String TAG = "SampleMeasureActivity";
     private BroadcastReceiver broadcastReceiver;
-    private String tabletLat;
-    private String tabletLon;
+    private Double tabletLat;
+    private Double tabletLon;
     private ArrayList<String> selectedDeviceAttributes;
     private String deviceSelectedName;
     private final int deviceIDIndex = 0;
     private final int deviceFullNameIndex = 1;
     private final int deviceTypeIndex = 2;
+    private double originLatitude;
+    private double originLongitude;
+    private int originMMSI;
+    private double beta;
+    private double distance;
+    private double alpha;
+    private double xPosition;
+    private double yPosition;
+    private double theta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +56,7 @@ public class SampleMeasurementActivity extends Activity {
 
 
         //Advanced Search Feature
+        DatabaseHelper.loadDeviceList(getApplicationContext()); //only for debugging purpose
         AutoCompleteTextView deviceNameTextView = findViewById(R.id.deviceshortname);
         ArrayAdapter<String> adapter = DatabaseHelper.advancedSearchTextView(getApplicationContext());
         deviceNameTextView.setAdapter(adapter);
@@ -56,8 +68,8 @@ public class SampleMeasurementActivity extends Activity {
                 public void onReceive(Context context, Intent intent) {
 
                     if(intent.getExtras()!= null) {
-                        tabletLat = intent.getExtras().get(GPS_Service.latitude).toString();
-                        tabletLon = intent.getExtras().get(GPS_Service.longitude).toString();
+                        tabletLat = intent.getExtras().getDouble(GPS_Service.latitude);
+                        tabletLon = intent.getExtras().getDouble(GPS_Service.longitude);
                         populateTabLocation();
                     }
                 }
@@ -101,8 +113,8 @@ public class SampleMeasurementActivity extends Activity {
         TextView latView = findViewById(R.id.tabLat);
         TextView lonView = findViewById(R.id.tabLon);
 
-        latView.setText(tabletLat);
-        lonView.setText(tabletLon);
+        latView.setText(String.valueOf(tabletLat));
+        lonView.setText(String.valueOf(tabletLon));
     }
 
     private void populateDeviceAttributes(){
@@ -121,14 +133,88 @@ public class SampleMeasurementActivity extends Activity {
         SQLiteOpenHelper dbHelper = DatabaseHelper.getDbInstance(getApplicationContext());
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        ContentValues mContentValues = new ContentValues();
-        mContentValues.put(DatabaseHelper.deviceID, selectedDeviceAttributes.get(deviceIDIndex));
-        mContentValues.put(DatabaseHelper.deviceName, selectedDeviceAttributes.get(deviceFullNameIndex));
-        mContentValues.put(DatabaseHelper.deviceShortName, deviceSelectedName);
-        mContentValues.put(DatabaseHelper.deviceType, selectedDeviceAttributes.get(deviceTypeIndex));
-        mContentValues.put(DatabaseHelper.latitude, tabletLat);
-        mContentValues.put(DatabaseHelper.longitude, tabletLon);
-        mContentValues.put(DatabaseHelper.updateTime, SystemClock.elapsedRealtime());
-        db.insert(DatabaseHelper.sampleMeasurementTable, null, mContentValues);
+        if(getOriginCoordinates()) {
+            calculateSampledLocationParameters();
+            ContentValues mContentValues = new ContentValues();
+            mContentValues.put(DatabaseHelper.deviceID, selectedDeviceAttributes.get(deviceIDIndex));
+            mContentValues.put(DatabaseHelper.deviceName, selectedDeviceAttributes.get(deviceFullNameIndex));
+            mContentValues.put(DatabaseHelper.deviceShortName, deviceSelectedName);
+            mContentValues.put(DatabaseHelper.deviceType, selectedDeviceAttributes.get(deviceTypeIndex));
+            mContentValues.put(DatabaseHelper.latitude, tabletLat);
+            mContentValues.put(DatabaseHelper.longitude, tabletLon);
+            mContentValues.put(DatabaseHelper.xPosition, xPosition);
+            mContentValues.put(DatabaseHelper.yPosition, yPosition);
+            mContentValues.put(DatabaseHelper.updateTime, SystemClock.elapsedRealtime());
+            db.insert(DatabaseHelper.sampleMeasurementTable, null, mContentValues);
+        }else {
+            Log.d(TAG, "Error Inserting new data");
+        }
+
     }
+
+    private void calculateSampledLocationParameters(){
+        distance = NavigationFunctions.calculateDifference(tabletLat, tabletLon, originLatitude, originLongitude);
+        theta = NavigationFunctions.calculateAngleBeta(tabletLat, tabletLon, originLatitude, originLongitude);
+        alpha = Math.abs(theta - beta);
+        xPosition = distance * Math.cos(Math.toRadians(alpha));
+        yPosition = distance * Math.sin(Math.toRadians(alpha));
+    }
+
+    private boolean getOriginCoordinates(){
+
+        try {
+            DatabaseHelper databaseHelper = DatabaseHelper.getDbInstance(getApplicationContext());
+            SQLiteDatabase db = databaseHelper.getReadableDatabase();
+            Cursor baseStationCursor = db.query(DatabaseHelper.baseStationTable,
+                    new String[] {DatabaseHelper.mmsi},
+                    null,
+                    null,
+                    null, null, null);
+            if (baseStationCursor.getCount() != DatabaseHelper.INITIALIZATION_SIZE){
+                Log.d(TAG, "Error Reading from BaseStation Table");
+                return false;
+            } else{
+                if(baseStationCursor.moveToFirst()){
+                    originMMSI = baseStationCursor.getInt(baseStationCursor.getColumnIndex(DatabaseHelper.mmsi));
+                }
+            }
+            Cursor fixedStationCursor = db.query(DatabaseHelper.fixedStationTable,
+                    new String[] {DatabaseHelper.latitude, DatabaseHelper.longitude},
+                    DatabaseHelper.mmsi +" = ?",
+                    new String[] {String.valueOf(originMMSI)},
+                    null, null, null);
+            if (fixedStationCursor.getCount() != 1){
+                Log.d(TAG, "Error Reading Origin Latitude Longitude");
+                return false;
+            } else{
+                if(fixedStationCursor.moveToFirst()){
+                    originLatitude = fixedStationCursor.getDouble(fixedStationCursor.getColumnIndex(DatabaseHelper.latitude));
+                    originLongitude = fixedStationCursor.getDouble(fixedStationCursor.getColumnIndex(DatabaseHelper.longitude));
+                }
+            }
+
+            Cursor betaCursor = db.query(DatabaseHelper.betaTable,
+                    new String[]{DatabaseHelper.beta, DatabaseHelper.updateTime},
+                    null, null,
+                    null, null, null);
+            if (betaCursor.getCount() == 1) {
+                if (betaCursor.moveToFirst()) {
+                    beta = betaCursor.getDouble(betaCursor.getColumnIndex(DatabaseHelper.beta));
+                }
+
+            } else {
+                Log.d(TAG, "Error in Beta Table");
+                return false;
+            }
+            betaCursor.close();
+            baseStationCursor.close();
+            fixedStationCursor.close();
+            return true;
+        } catch(SQLiteException e){
+            Log.d(TAG, "Database Error");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
